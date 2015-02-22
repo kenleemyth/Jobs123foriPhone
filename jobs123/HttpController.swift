c89a1f0a8f7d616edecb36ebbee0ea5e7e996428
Myth <28499631@qq.com>
2015-02-22 20:35:14 +0800

    修正格式
    修改了格式

--- jobs123/HttpController.swift
+++ jobs123/HttpController.swift
@@ -12,14 +12,7 @@ class HttpController:NSObject{
     func OnSearch(url:String){
         var nsUrl:NSURL=NSURL(string: url)!
         var request:NSURLRequest=NSURLRequest(URL: nsUrl)
-        NSURLConnection.sendAsynchronousRequest(
-            request,
-            queue: NSOperationQueue.mainQueue(),
-            completionHandler:
-            {(response:NSURLResponse!,
-                data:NSData!,
-                error:NSError!)
-                ->Void in
+        NSURLConnection.sendAsynchronousRequest(request,queue: NSOperationQueue.mainQueue(),completionHandler:{(response:NSURLResponse!,data:NSData!,error:NSError!)->Void in
         
             var jsonResult:NSDictionary=NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
         
